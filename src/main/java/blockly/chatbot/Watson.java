4d488113c9f23e6e3af9724a930daf7a278a0dd4
Igor Andrade <igor.andrade@techne.com.br>
2023-09-27 15:39:04 +0000

    Ajustes

--- src/main/java/blockly/chatbot/Watson.java
+++ src/main/java/blockly/chatbot/Watson.java
@@ -5,7 +5,6 @@ import cronapi.rest.security.CronappSecurity;
 import java.util.concurrent.Callable;
 
 
-
 @CronapiMetaData(type = "blockly")
 @CronappSecurity(post = "Public", get = "Public", execute = "Public", delete = "Public", put = "Public")
 public class Watson {
@@ -14,61 +13,31 @@ public static final int TIMEOUT = 300;
 
 /**
  *
- * @return Var
- */
-// Watson
-public static Var iniciar() throws Exception {
- return new Callable<Var>() {
-
-   private Var watson = Var.VAR_NULL;
-
-   public Var call() throws Exception {
-
-    watson =
-    Var.valueOf(cronapi.watson.conversation.ConversationOperations.message(
-    Var.valueOf("2018-02-16").getTypedObject(java.lang.String.class),
-    Var.valueOf("apikey").getTypedObject(java.lang.String.class),
-    Var.valueOf("xL1nLIhheJNk7U_kCOR8anA-U5LSYO04FIaJWcamWLV6").getTypedObject(java.lang.String.class),
-    Var.VAR_NULL.getTypedObject(java.lang.String.class), Var.VAR_NULL.getTypedObject(java.util.Map.class),
-    cronapi.object.Operations.newObject(Var.valueOf("com.ibm.watson.developer_cloud.conversation.v1.model.MessageOptions"),Var.valueOf("workspaceId",
-    Var.valueOf("2b8372ac-2843-4a6c-938c-a6abf02db247"))).getTypedObject(com.ibm.watson.developer_cloud.conversation.v1.model.MessageOptions.class)));
-    return
-cronapi.map.Operations.createObjectMapWith(Var.valueOf("context",
-cronapi.object.Operations.getObjectField(watson,
-Var.valueOf("context"))) , Var.valueOf("texto",
-cronapi.object.Operations.getObjectField(
-cronapi.object.Operations.getObjectField(watson,
-Var.valueOf("output")),
-Var.valueOf("text[0]"))) , Var.valueOf("user",
-Var.valueOf("PharmaBot")));
-   }
- }.call();
-}
-
-/**
+ * Descreva esta função...
  *
  * @param context
  * @param msg
- * @return Var
+ *
+ * @author Igor Andrade
+ * @since 26/09/2023, 09:48:15
+ *
  */
-// Descreva esta função...
-public static Var enviarMensagem(Var context, Var msg) throws Exception {
+public static Var enviarMensagem(@ParamMetaData(description = "context", id = "c5d5631e") Var context, @ParamMetaData(description = "msg", id = "69c2571a") Var msg) throws Exception {
  return new Callable<Var>() {
 
    private Var watson = Var.VAR_NULL;
 
    public Var call() throws Exception {
-
+    // Exemplo de função para enviar mensagem para o Watson Assistant.
     watson =
     Var.valueOf(cronapi.watson.conversation.ConversationOperations.message(
     Var.valueOf("2018-02-16").getTypedObject(java.lang.String.class),
     Var.valueOf("apikey").getTypedObject(java.lang.String.class),
     Var.valueOf("xL1nLIhheJNk7U_kCOR8anA-U5LSYO04FIaJWcamWLV6").getTypedObject(java.lang.String.class),
-    Var.VAR_NULL.getTypedObject(java.lang.String.class), Var.VAR_NULL.getTypedObject(java.util.Map.class),
+    Var.valueOf("").getTypedObject(java.lang.String.class), Var.VAR_NULL.getTypedObject(java.util.Map.class),
     cronapi.object.Operations.newObject(Var.valueOf("com.ibm.watson.developer_cloud.conversation.v1.model.MessageOptions"),Var.valueOf("workspaceId",
     Var.valueOf("2b8372ac-2843-4a6c-938c-a6abf02db247")),Var.valueOf("input",
     cronapi.object.Operations.newObject(Var.valueOf("com.ibm.watson.developer_cloud.conversation.v1.model.InputData"),Var.valueOf("text",msg))),Var.valueOf("context",context)).getTypedObject(com.ibm.watson.developer_cloud.conversation.v1.model.MessageOptions.class)));
-
     watson =
     Var.valueOf(filtarIntencao(watson));
     return
@@ -86,11 +55,15 @@ Var.valueOf("chatbot")));
 
 /**
  *
- * @param param_watson
- * @return Var
+ * Descreva esta função...
+ *
+ * @param watson
+ *
+ * @author Igor Andrade
+ * @since 26/09/2023, 09:48:15
+ *
  */
-// Descreva esta função...
-public static Var filtarIntencao(Var param_watson) throws Exception {
+public static Var filtarIntencao(@ParamMetaData(description = "param_watson", id = "a5bcd5e2") Var param_watson) throws Exception {
  return new Callable<Var>() {
 
    // param
@@ -102,131 +75,146 @@ public static Var filtarIntencao(Var param_watson) throws Exception {
    private Var lista = Var.VAR_NULL;
 
    public Var call() throws Exception {
-
+    // Exemplo de função para gerenciar a fila de requisição para o Watson Assistant.
     intencoes =
     cronapi.object.Operations.getObjectField(watson,
     Var.valueOf("intents"));
-
     context =
     cronapi.object.Operations.getObjectField(watson,
     Var.valueOf("context"));
-
     if (
-    /*# sourceMappingStart=M1!v9D_gN_#KQh`3|OZI */
-    cronapi.logic.Operations.isNullOrEmpty(intencoes).negate().getObjectAsBoolean()) {
-
+    cronapi.logic.Operations.isNullOrEmpty(intencoes)
+    .negate().getObjectAsBoolean()) {
         System.out.println(
         Var.valueOf("listar_produtos").getObjectAsString());
-
         intencao =
         cronapi.list.Operations.getFirst(intencoes);
-
         if (
         Var.valueOf(
         Var.valueOf("listar_produtos").equals(
         cronapi.object.Operations.getObjectField(intencao,
         Var.valueOf("intent")))).getObjectAsBoolean()) {
-
             cronapi.json.Operations.setJsonOrMapField(context,
             Var.valueOf("listaProdutos"),
-            blockly.Produto.listarProdutos());
-
+            cronapi.util.Operations.callBlockly(Var.valueOf("blockly.Produto:listarProdutos")));
             watson =
             Var.valueOf(cronapi.watson.conversation.ConversationOperations.message(
             Var.valueOf("2018-02-16").getTypedObject(java.lang.String.class),
             Var.valueOf("apikey").getTypedObject(java.lang.String.class),
             Var.valueOf("xL1nLIhheJNk7U_kCOR8anA-U5LSYO04FIaJWcamWLV6").getTypedObject(java.lang.String.class),
-            Var.VAR_NULL.getTypedObject(java.lang.String.class), Var.VAR_NULL.getTypedObject(java.util.Map.class),
+            Var.valueOf("").getTypedObject(java.lang.String.class), Var.VAR_NULL.getTypedObject(java.util.Map.class),
             cronapi.object.Operations.newObject(Var.valueOf("com.ibm.watson.developer_cloud.conversation.v1.model.MessageOptions"),Var.valueOf("workspaceId",
             Var.valueOf("2b8372ac-2843-4a6c-938c-a6abf02db247")),Var.valueOf("context",context)).getTypedObject(com.ibm.watson.developer_cloud.conversation.v1.model.MessageOptions.class)));
         }
     }
-
     if (
     Var.valueOf(
     cronapi.object.Operations.getObjectField(context,
     Var.valueOf("comprarProdutos")).getObjectAsBoolean() &&
-    /*# sourceMappingStart=dtU+7nvTTMq#au:QTe#s */
     cronapi.logic.Operations.isNullOrEmpty(
     cronapi.object.Operations.getObjectField(context,
-    Var.valueOf("codProduto"))).negate().getObjectAsBoolean()).getObjectAsBoolean()) {
-
+    Var.valueOf("codProduto")))
+    .negate().getObjectAsBoolean()).getObjectAsBoolean()) {
         if (
-        /*# sourceMappingStart=87@fd1][KSl/6VEV9!Qx */
         cronapi.logic.Operations.isNullOrEmpty(
         cronapi.object.Operations.getObjectField(context,
-        Var.valueOf("produtosCarrinho"))).negate().getObjectAsBoolean()) {
-
+        Var.valueOf("produtosCarrinho")))
+        .negate().getObjectAsBoolean()) {
             lista =
             cronapi.object.Operations.getObjectField(context,
             Var.valueOf("produtosCarrinho"));
-
             cronapi.list.Operations.addLast(lista,
             cronapi.object.Operations.getObjectField(context,
             Var.valueOf("codProduto")));
-
             cronapi.json.Operations.setJsonOrMapField(context,
             Var.valueOf("produtosCarrinho"), lista);
         } else {
-
             cronapi.json.Operations.setJsonOrMapField(context,
             Var.valueOf("produtosCarrinho"),
             cronapi.list.Operations.newList(
             cronapi.object.Operations.getObjectField(context,
             Var.valueOf("codProduto"))));
         }
-
         cronapi.json.Operations.setJsonOrMapField(context,
         Var.valueOf("produtoAdicionado"),
         Var.VAR_TRUE);
-
         cronapi.json.Operations.setJsonOrMapField(context,
         Var.valueOf("codProduto"),
         Var.VAR_NULL);
-
         watson =
         Var.valueOf(cronapi.watson.conversation.ConversationOperations.message(
         Var.valueOf("2018-02-16").getTypedObject(java.lang.String.class),
         Var.valueOf("apikey").getTypedObject(java.lang.String.class),
         Var.valueOf("xL1nLIhheJNk7U_kCOR8anA-U5LSYO04FIaJWcamWLV6").getTypedObject(java.lang.String.class),
-        Var.VAR_NULL.getTypedObject(java.lang.String.class), Var.VAR_NULL.getTypedObject(java.util.Map.class),
+        Var.valueOf("").getTypedObject(java.lang.String.class), Var.VAR_NULL.getTypedObject(java.util.Map.class),
         cronapi.object.Operations.newObject(Var.valueOf("com.ibm.watson.developer_cloud.conversation.v1.model.MessageOptions"),Var.valueOf("workspaceId",
         Var.valueOf("2b8372ac-2843-4a6c-938c-a6abf02db247")),Var.valueOf("context",context)).getTypedObject(com.ibm.watson.developer_cloud.conversation.v1.model.MessageOptions.class)));
     }
-
     if (
     Var.valueOf(
     cronapi.object.Operations.getObjectField(context,
     Var.valueOf("finalizarCompra")).getObjectAsBoolean() &&
-    /*# sourceMappingStart=:!3g},33]Cb6`?yvu9~* */
     cronapi.logic.Operations.isNullOrEmpty(
     cronapi.object.Operations.getObjectField(context,
-    Var.valueOf("cpf"))).negate().getObjectAsBoolean()).getObjectAsBoolean()) {
-
+    Var.valueOf("cpf")))
+    .negate().getObjectAsBoolean()).getObjectAsBoolean()) {
         context =
-        blockly.Pedido.salvarItensChatBot(context);
-
+        cronapi.util.Operations.callBlockly(Var.valueOf("blockly.Pedido:salvarItensChatBot"), context);
         watson =
         Var.valueOf(cronapi.watson.conversation.ConversationOperations.message(
         Var.valueOf("2018-02-16").getTypedObject(java.lang.String.class),
         Var.valueOf("apikey").getTypedObject(java.lang.String.class),
         Var.valueOf("xL1nLIhheJNk7U_kCOR8anA-U5LSYO04FIaJWcamWLV6").getTypedObject(java.lang.String.class),
-        Var.VAR_NULL.getTypedObject(java.lang.String.class), Var.VAR_NULL.getTypedObject(java.util.Map.class),
+        Var.valueOf("").getTypedObject(java.lang.String.class), Var.VAR_NULL.getTypedObject(java.util.Map.class),
         cronapi.object.Operations.newObject(Var.valueOf("com.ibm.watson.developer_cloud.conversation.v1.model.MessageOptions"),Var.valueOf("workspaceId",
         Var.valueOf("2b8372ac-2843-4a6c-938c-a6abf02db247")),Var.valueOf("context",context)).getTypedObject(com.ibm.watson.developer_cloud.conversation.v1.model.MessageOptions.class)));
-
         if (
-        /*# sourceMappingStart=`}r_B-%fcVG$:DxACSq+ */
         cronapi.logic.Operations.isNullOrEmpty(
         cronapi.json.Operations.getJsonOrMapField(context,
-        Var.valueOf("idVenda"))).negate().getObjectAsBoolean()) {
-
-            blockly.Boleto.emitir(
+        Var.valueOf("idVenda")))
+        .negate().getObjectAsBoolean()) {
+            cronapi.util.Operations.callBlockly(Var.valueOf("blockly.Boleto:emitir"),
             cronapi.json.Operations.getJsonOrMapField(context,
             Var.valueOf("idVenda")));
         }
     }
     return watson;
+   }
+ }.call();
+}
+
+/**
+ *
+ * Watson
+ *
+ * @author Igor Andrade
+ * @since 26/09/2023, 09:48:15
+ *
+ */
+public static Var iniciar() throws Exception {
+ return new Callable<Var>() {
+
+   private Var watson = Var.VAR_NULL;
+
+   public Var call() throws Exception {
+    // Exemplo de função para iniciar o Watson Assistant.
+    watson =
+    Var.valueOf(cronapi.watson.conversation.ConversationOperations.message(
+    Var.valueOf("2018-02-16").getTypedObject(java.lang.String.class),
+    Var.valueOf("apikey").getTypedObject(java.lang.String.class),
+    Var.valueOf("xL1nLIhheJNk7U_kCOR8anA-U5LSYO04FIaJWcamWLV6").getTypedObject(java.lang.String.class),
+    Var.valueOf("").getTypedObject(java.lang.String.class), Var.VAR_NULL.getTypedObject(java.util.Map.class),
+    cronapi.object.Operations.newObject(Var.valueOf("com.ibm.watson.developer_cloud.conversation.v1.model.MessageOptions"),Var.valueOf("workspaceId",
+    Var.valueOf("2b8372ac-2843-4a6c-938c-a6abf02db247"))).getTypedObject(com.ibm.watson.developer_cloud.conversation.v1.model.MessageOptions.class)));
+    return
+cronapi.map.Operations.createObjectMapWith(Var.valueOf("context",
+cronapi.object.Operations.getObjectField(watson,
+Var.valueOf("context"))) , Var.valueOf("texto",
+cronapi.object.Operations.getObjectField(
+cronapi.object.Operations.getObjectField(watson,
+Var.valueOf("output")),
+Var.valueOf("text[0]"))) , Var.valueOf("user",
+Var.valueOf("PharmaBot")));
    }
  }.call();
 }
