89da569cd2d22c7c8b8dae26325876b1621a838d
Alan Moraes Souza <alan.souza@cronapp.io>
2023-11-29 15:15:17 -0300

    CRONAPP-11304 Projeto farmácia está quebrado no mobile

--- src/main/mobileapp/www/js/blockly/Pedido.blockly.js
+++ src/main/mobileapp/www/js/blockly/Pedido.blockly.js
@@ -4,95 +4,182 @@ window.blockly.js.blockly = window.blockly.js.blockly || {};
 window.blockly.js.blockly.Pedido = window.blockly.js.blockly.Pedido || {};
 
 /**
+ * @function finalizar
+ *
  * Pedido
+ *
+ *
+ * @author Root
+ * @since 29/11/2023, 15:05:21
+ *
  */
+window.blockly.js.blockly.Pedido.finalizarArgs = [];
 window.blockly.js.blockly.Pedido.finalizar = async function() {
- var item, resposta, index, quantidade, produto, totalItem, codigo;
+
+  //
   this.cronapi.util.callServerBlocklyAsynchronous('blockly.Pedido:salvarItens', async function(sender_resposta) {
       resposta = sender_resposta;
+    //
     if (resposta) {
+      //
       this.cronapi.screen.changeView("#/app/logged/venda",[  ]);
     }
   }.bind(this), this.cronapi.screen.getScopeVariable('listaItensPedido'), this.cronapi.screen.getScopeVariable('cliente'), null);
 }
 
 /**
+ * @function limparVariaveis
+ *
  * Descreva esta função...
+ *
+ *
+ * @author Root
+ * @since 29/11/2023, 15:05:21
+ *
  */
+window.blockly.js.blockly.Pedido.limparVariaveisArgs = [];
 window.blockly.js.blockly.Pedido.limparVariaveis = async function() {
- var item, resposta, index, quantidade, produto, totalItem, codigo;
+
+  //
   this.cronapi.screen.changeValueOfField("vars.produto", null);
-  this.cronapi.screen.changeValueOfField("vars.quantidade", null);
+  //
+  this.cronapi.screen.changeValueOfField("vars.quantidade", 0);
 }
 
 /**
+ * @function criarLista
+ *
  * Descreva esta função...
+ *
+ *
+ * @author Root
+ * @since 29/11/2023, 15:05:21
+ *
  */
+window.blockly.js.blockly.Pedido.criarListaArgs = [];
 window.blockly.js.blockly.Pedido.criarLista = async function() {
- var item, resposta, index, quantidade, produto, totalItem, codigo;
+
+  //
   this.cronapi.screen.createScopeVariable('listaItensPedido', []);
+  //
   this.cronapi.screen.createScopeVariable('valorTotal', 0);
 }
 
 /**
+ * @function excluir
+ *
  * Descreva esta função...
+ *
+ * @param index
+ *
+ * @author Root
+ * @since 29/11/2023, 15:05:21
+ *
  */
+window.blockly.js.blockly.Pedido.excluirArgs = [{ description: 'index', id: '75a75b87' }];
 window.blockly.js.blockly.Pedido.excluir = async function(index) {
- var item, resposta, quantidade, produto, totalItem, codigo;
+
+  //
   this.cronapi.screen.getScopeVariable('listaItensPedido').splice((index - 1), 1);
-  await this.blockly.js.blockly.Pedido.calcularTotal();
+  //
+  (await this.cronapi.client('blockly.js.blockly.Pedido.calcularTotal').run());
 }
 
 /**
+ * @function inserir
+ *
  * Descreva esta função...
+ *
+ * @param produto
+ * @param quantidade
+ *
+ * @author Root
+ * @since 29/11/2023, 15:05:21
+ *
  */
+window.blockly.js.blockly.Pedido.inserirArgs = [{ description: 'produto', id: 'abf72696' }, { description: 'quantidade', id: 'fbb6d20a' }];
 window.blockly.js.blockly.Pedido.inserir = async function(produto, quantidade) {
- var item, resposta, index, totalItem, codigo;
+
+  //
   if (!this.cronapi.logic.isNullOrEmpty(produto) && !this.cronapi.logic.isNullOrEmpty(quantidade)) {
+    //
     if (quantidade > 0) {
+      //
       item = this.cronapi.object.newObject();
+      //
       this.cronapi.object.setProperty(item, 'produto', produto);
+      //
       this.cronapi.object.setProperty(item, 'quantidade', quantidade);
+      //
       this.cronapi.screen.getScopeVariable('listaItensPedido').push(item);
-      await this.blockly.js.blockly.Pedido.limparVariaveis();
-      await this.blockly.js.blockly.Pedido.calcularTotal();
+      //
+      (await this.cronapi.client('blockly.js.blockly.Pedido.limparVariaveis').run());
+      //
+      (await this.cronapi.client('blockly.js.blockly.Pedido.calcularTotal').run());
     } else {
+      //
       this.cronapi.screen.notify('warning','Quantidade tem que ser maior que 0');
     }
   }
 }
 
 /**
+ * @function calcularTotal
+ *
  * Descreva esta função...
+ *
+ *
+ * @author Root
+ * @since 29/11/2023, 15:05:21
+ *
  */
+window.blockly.js.blockly.Pedido.calcularTotalArgs = [];
 window.blockly.js.blockly.Pedido.calcularTotal = async function() {
- var item, resposta, index, quantidade, produto, totalItem, codigo;
+
+  //
   this.cronapi.screen.changeValueOfField("vars.valorTotal", 0);
+  //
   var produto_list = this.cronapi.screen.getScopeVariable('listaItensPedido');
   for (var produto_index in produto_list) {
     produto = produto_list[produto_index];
-    totalItem = this.cronapi.object.getProperty(produto, 'produto.precoVenda') * this.cronapi.object.getProperty(produto, 'quantidade');
-    this.cronapi.screen.changeValueOfField("vars.valorTotal", this.cronapi.screen.getValueOfField("vars.valorTotal") + totalItem);
+    //
+    totalItem = (this.cronapi.object.getProperty(produto, 'produto.precoVenda') * this.cronapi.object.getProperty(produto, 'quantidade'));
+    //
+    this.cronapi.screen.changeValueOfField("vars.valorTotal", (this.cronapi.screen.getValueOfField("vars.valorTotal") + totalItem));
   }
 }
 
 /**
+ * @function lerCodigoBarras
+ *
  * Descreva esta função...
+ *
+ *
+ * @author Root
+ * @since 29/11/2023, 15:05:21
+ *
  */
+window.blockly.js.blockly.Pedido.lerCodigoBarrasArgs = [];
 window.blockly.js.blockly.Pedido.lerCodigoBarras = async function() {
- var item, resposta, index, quantidade, produto, totalItem, codigo;
+
+  //
   this.cronapi.cordova.camera.qrCodeScanner('CODE_39', 'Consultar o preço do produto', async function(sender_codigo) {
       codigo = sender_codigo;
+    //
     this.cronapi.util.callServerBlocklyAsynchronous('blockly.Produto:consultar', async function(sender_item) {
         item = sender_item;
+      //
       if (!this.cronapi.logic.isNullOrEmpty(this.cronapi.object.getProperty(item, 'msg'))) {
+        //
         this.cronapi.screen.notify('error',this.cronapi.object.getProperty(item, 'msg'));
       } else {
+        //
         this.cronapi.screen.notify('info',[this.cronapi.object.getProperty(item, 'nome'),' - ',this.cronapi.object.getProperty(item, 'precoCusto')].join(''));
       }
     }.bind(this), codigo);
   }.bind(this), async function(sender_item) {
       item = sender_item;
+    //
     this.cronapi.screen.notify('error',item);
   }.bind(this));
 }
